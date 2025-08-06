cf7991a2926ff69ca5968303405eec4fa181f2d4
Aldebaran49 <andreev470@bk.ru>
2025-08-06 22:19:10 +0300

    test commit

--- src/test/java/ru/andreev/auction/service/UserServiceTest.java
+++ src/test/java/ru/andreev/auction/service/UserServiceTest.java
@@ -19,6 +19,7 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+//Unit tests
 @ExtendWith(MockitoExtension.class)
 public class UserServiceTest {
 
